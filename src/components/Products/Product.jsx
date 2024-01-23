a73757a1824507567f6f367b47dbfadab577a668
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-23 15:37:06 +0500

    Refactor Product component to fetch and display filtered products

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -1,28 +1,47 @@
 import React from 'react'
 import Card from './Card'
+import { fetchProducts } from '../../app/features/products'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
 
 const cardContainer = {
   display: 'flex',
   flexWrap: 'wrap',
   justifyContent: 'space-around',
-  gap : '4%',
-  paddingTop: '7%',
+  gap : '3%',
+  padding: '7% 3% 7% 3%',
+  height: '100%'
 }
 
 const Product = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(fetchProducts())
+        console.log('fetching products')
+    }, [dispatch])
+    const products = useSelector((state) =>
+    state.products.products
+      .filter((product) => product.inventory > 0)
+      .map((product) => ({
+        id: product._id,
+        name: product.name,
+        price: product.price,
+        quantity: product.inventory,
+      }))
+  );
+
+    console.log(products)
   return (
     <div style={cardContainer}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {products.map((product) => (
+            <Card
+            key={product._id}
+            id={product._id}
+            name={product.name}
+            price={product.price}
+            quantity={product.quantity}
+            />
+        ))}
     </div>
   )
 }
