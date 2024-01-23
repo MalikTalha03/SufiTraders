25f633e9c611010715e8d3a5d92b1c9c0f80131f
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-23 12:01:38 +0500

    Add routing and new components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Card from './components/Products/Card';
+import Product from './components/Products/Product';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path="/" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
