88ef26a1e0ce989b197538f477eef44ca68b057d
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-24 00:35:49 +0500

    Add search functionality to Product component

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -2,48 +2,74 @@ import React from 'react'
 import Card from './Card'
 import { fetchProducts } from '../../app/features/products'
 import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { Input } from 'antd'
 
 const cardContainer = {
   display: 'flex',
   flexWrap: 'wrap',
   justifyContent: 'space-around',
   gap : '3%',
-  padding: '7% 3% 7% 3%',
+  padding: '1% 3% 7% 3%',
   height: '100%'
 }
 
-const Product = () => {
-    const dispatch = useDispatch()
+const searchBar = {
+    width: "15%",
+    height: "5%",
+    marginTop: "6%",
+    alignSelf: "flex-end",
+    marginRight: "5%",
+  };
+
+  const Product = () => {
+    const dispatch = useDispatch();
     useEffect(() => {
-        dispatch(fetchProducts())
-        console.log('fetching products')
-    }, [dispatch])
-    const products = useSelector((state) =>
-    state.products.products
-      .filter((product) => product.inventory > 0)
-      .map((product) => ({
+      dispatch(fetchProducts());
+      console.log('fetching products');
+    }, [dispatch]);
+  
+    const allProducts = useSelector((state) =>
+      state.products.products.map((product) => ({
         id: product._id,
         name: product.name,
         price: product.price,
         quantity: product.inventory,
       }))
-  );
-
-    console.log(products)
-  return (
-    <div style={cardContainer}>
-        {products.map((product) => (
+    );
+  
+    const [searchValue, setSearchValue] = useState('');
+    
+    const filteredProducts = allProducts.filter(
+      (product) =>
+        product.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+  
+  
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+        <Input
+          placeholder="Search Product"
+          style={searchBar}
+            onChange={(e) => setSearchValue(e.target.value)}
+        />
+        <div style={cardContainer}>
+        {filteredProducts.length === 0 ? (
+          <p>No products found</p>
+        ) : (
+          filteredProducts.map((product) => (
             <Card
-            key={product._id}
-            id={product._id}
-            name={product.name}
-            price={product.price}
-            quantity={product.quantity}
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              price={product.price}
+              quantity={product.quantity}
             />
-        ))}
-    </div>
-  )
-}
-
-export default Product
+          ))
+        )}
+        </div>
+      </div>
+    );
+  };
+  
+  export default Product;
