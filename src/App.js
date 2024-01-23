a4855838b94a024576c296f6e6636c566dea0a2a
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-23 15:36:27 +0500

    Add div wrapper to App component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import Card from './components/Products/Card';
 import Product from './components/Products/Product';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-
 function App() {
   return (
+    <div className="App">
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Product />} />
       </Routes>
     </BrowserRouter>
+    </div>
   );
 }
 
