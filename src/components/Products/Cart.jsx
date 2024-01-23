a8eb5ab5bed0de8a7f860e4bf17f39fa1521850b
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-24 00:35:45 +0500

    Add CartDrawer component and functionality

--- src/components/Products/Cart.jsx
+++ src/components/Products/Cart.jsx
@@ -2,8 +2,18 @@ import React, { useState, useEffect } from "react";
 import { Avatar, Badge, Button, Space } from "antd";
 import { useSelector } from "react-redux";
 import * as FaIcons from "react-icons/fa";
+import CartDrawer from "./CartDrawer";
 const Cart = (props) => {
   const [count, setCount] = useState(0);
+  const [cartVisible, setCartVisible] = useState(false);
+
+  const openCartDrawer = () => {
+    setCartVisible(true);
+  };
+
+  const closeCartDrawer = () => {
+    setCartVisible(false);
+  };
   const orderdata = useSelector((state) => {
     const data = state.orderdata.orderdata;
     return data.filter((item) => item.quantity > 0);
@@ -16,8 +26,9 @@ const Cart = (props) => {
   return (
     <Space direction="vertical" style={props.style}>
       <Badge count={count}>
-        <FaIcons.FaShoppingCart size={30} color="black" />
+        <FaIcons.FaShoppingCart size={30} color="black" onClick={openCartDrawer} />
       </Badge>
+      <CartDrawer visible={cartVisible} onClose={closeCartDrawer} />
     </Space>
   );
 };
