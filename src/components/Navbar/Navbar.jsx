144357d4dec4e725d508d17cf609f6f3cc3ccbf1
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-24 13:41:05 +0500

    Refactor Navbar component to include a responsive menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,8 @@
 import React, { useState } from "react";
-import { Menu, Space, Typography, Input } from "antd";
+import { Menu, Typography, Drawer } from "antd";
+import { Link } from "react-router-dom";
 import Cart from "../Products/Cart";
-
-const navBarStyle = {
-  backgroundColor: "#D4E7C5",
-  position: "fixed",
-  width: "100%",
-  zIndex: "999",
-  height: "7%",
-};
-
-const menuStyle = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "space-between",
-};
+import { MenuOutlined } from "@ant-design/icons";
 
 const itemStyle = {
   margin: "0 10px",
@@ -25,30 +13,24 @@ const itemStyle = {
   background: "none",
 };
 
-const items = {
-  display: "flex",
-  alignItems: "center",
-  width: "50%",
-  marginLeft: "25%",
-};
-
-const logoStyle = {
-  width: "5%",
-  height: "5%",
-  marginRight: "5%",
-  border: "2px solid #1890ff",
-  background: "none",
-};
-
 const defaultColor = "#000";
 const hoverColor = "#1890ff";
 
 const Navbar = () => {
+  const [visible, setVisible] = useState(false);
   const [itemColors, setItemColors] = useState({
     app: defaultColor,
     alipay1: defaultColor,
     alipay2: defaultColor,
   });
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
 
   const onItemMouseOver = (key) => {
     setItemColors((prevColors) => ({ ...prevColors, [key]: hoverColor }));
@@ -59,45 +41,102 @@ const Navbar = () => {
   };
 
   return (
-    <div style={navBarStyle}>
-      <Menu mode="horizontal" style={menuStyle}>
-        <div style={{ width: "50%", marginLeft: "5%" }}>
-          <Typography.Title level={3} style={{ margin: "0" }}>
+    <>
+      <div style={{ ...navBarStyle, justifyContent: "space-between" }}>
+        <div style={{ width: "30%", marginLeft: "5%" }}>
+          <Typography.Title
+            level={3}
+            style={{ margin: "0", fontSize: '1rem' }}
+          >
             Sufi Traders
           </Typography.Title>
         </div>
-        <div style={items}>
-          <Menu.Item
-            key="app"
-            style={{ ...itemStyle, color: itemColors.app }}
-            onMouseOver={() => onItemMouseOver("app")}
-            onMouseOut={() => onItemMouseOut("app")}
+        <div style={{ display: "none", cursor: "pointer" }}>
+          <MenuOutlined onClick={showDrawer} style={{ fontSize: "1.5rem" }} />
+        </div>
+        <div style={{ width: "50%", marginLeft: "25%" }}>
+          <Menu
+            mode="horizontal"
+            style={{ display: "flex", alignItems: "center" }}
           >
-            Home
-          </Menu.Item>
-          <Menu.Item
-            key="alipay1"
-            style={{ ...itemStyle, color: itemColors.alipay1 }}
-            onMouseOver={() => onItemMouseOver("alipay1")}
-            onMouseOut={() => onItemMouseOut("alipay1")}
-          >
-            About
-          </Menu.Item>
-          <Menu.Item
-            key="alipay2"
-            style={{ ...itemStyle, color: itemColors.alipay2 }}
-            onMouseOver={() => onItemMouseOver("alipay2")}
-            onMouseOut={() => onItemMouseOut("alipay2")}
-          >
-            Contact
-          </Menu.Item>
+            <Menu.Item key="app">
+              <Link
+                to="/"
+                style={{ ...itemStyle, color: itemColors.app }}
+                onMouseOver={() => onItemMouseOver("app")}
+                onMouseOut={() => onItemMouseOut("app")}
+              >
+                Home
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="alipay1">
+              <Link
+                to="/about"
+                style={{ ...itemStyle, color: itemColors.alipay1 }}
+                onMouseOver={() => onItemMouseOver("alipay1")}
+                onMouseOut={() => onItemMouseOut("alipay1")}
+              >
+                About
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="alipay2">
+              <Link
+                to="/contact"
+                style={{ ...itemStyle, color: itemColors.alipay2 }}
+                onMouseOver={() => onItemMouseOver("alipay2")}
+                onMouseOut={() => onItemMouseOut("alipay2")}
+              >
+                Contact
+              </Link>
+            </Menu.Item>
+          </Menu>
         </div>
         <Cart
-          style={{ marginRight: "3%", paddingTop: "1%", cursor: "pointer" }}
+          style={{
+            marginRight: "3%",
+            paddingTop: "1%",
+            cursor: "pointer",
+            fontSize: "1.5rem",
+          }}
         />
-      </Menu>
-    </div>
+      </div>
+
+      <Drawer
+        title="Menu"
+        placement="right"
+        onClose={onClose}
+        visible={visible}
+      >
+        <Menu mode="vertical">
+          <Menu.Item key="app">
+            <Link to="/" onClick={onClose}>
+              Home
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="alipay1">
+            <Link to="/about" onClick={onClose}>
+              About
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="alipay2">
+            <Link to="/contact" onClick={onClose}>
+              Contact
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </Drawer>
+    </>
   );
+};
+
+const navBarStyle = {
+  backgroundColor: "#fff",
+  position: "fixed",
+  width: "100%",
+  zIndex: "999",
+  height: "7%",
+  display: "flex",
+  alignItems: "center",
 };
 
 export default Navbar;
