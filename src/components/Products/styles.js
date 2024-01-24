70c1b5e1ac9af76070c619cf204c3a31808e54a6
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-24 12:45:28 +0500

    Add styles for Products component

---
+++ src/components/Products/styles.js
@@ -0,0 +1,126 @@
+const addCartBtn = {
+  marginTop: "10px",
+};
+
+const cardItems = {
+  marginTop: "10px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  alignItems: "center",
+};
+
+const cardMain = {
+  width: 240,
+  marginBottom: "3%",
+  backgroundColor: "#f5f5f5",
+};
+
+const qtySpan = {
+  margin: "0 10px",
+  fontSize: "16px",
+};
+
+const btnGrp = {
+  display: "flex",
+  alignItems: "center",
+};
+
+const nameSpan = {
+  fontWeight: "bold",
+  fontSize: "16px",
+};
+
+const cartBtn = {
+  background: "none",
+  border: "none",
+};
+
+const priceSpan = {
+  fontSize: "14px",
+  color: "#666",
+};
+
+const ordBtn = {
+  position: "absolute",
+  bottom: 16,
+  right: 16,
+};
+
+const span = {
+  fontWeight: "bold",
+  fontSize: "20px",
+};
+
+const table = {
+  marginTop: "10px",
+  display: "flex",
+  justifyContent: "flex-end",
+};
+
+const chkBtn = {
+  marginTop: "16px",
+  position: "absolute",
+  right: 16,
+  width: "35%",
+  height: "7%",
+};
+
+const subtotal = {
+  fontWeight: "bold",
+  fontSize: "20px",
+  marginTop: "16px",
+};
+
+const mainCont = {
+  width: "100%",
+  paddingRight: "16px",
+};
+
+const input = {
+  marginTop: "8px",
+};
+
+const cardContainer = {
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "space-around",
+  gap: "3%",
+  padding: "1% 3% 7% 3%",
+  height: "100%",
+};
+
+const searchBar = {
+  width: "15%",
+  height: "5%",
+  marginTop: "6%",
+  alignSelf: "flex-end",
+  marginRight: "5%",
+};
+
+const main = {
+  display: "flex",
+  flexDirection: "column",
+  height: "100%",
+};
+
+module.exports = {
+  addCartBtn,
+  cardItems,
+  cardMain,
+  qtySpan,
+  btnGrp,
+  nameSpan,
+  cartBtn,
+  priceSpan,
+  ordBtn,
+  span,
+  table,
+  chkBtn,
+  subtotal,
+  mainCont,
+  input,
+  cardContainer,
+  searchBar,
+  main,
+};
