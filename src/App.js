de9f7d6a8e615c39326f3a31a1b337cf2bd91b50
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-24 13:40:53 +0500

    Update routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import Card from './components/Products/Card';
 import Product from './components/Products/Product';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import CartDrawer from './components/Products/CartDrawer';
+import About from './components/About/About';
+import Contact from './components/About/Contact';
 
 function App() {
   return (
@@ -13,7 +12,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Product />} />
-        <Route path="/detail" element={<CartDrawer />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
     </div>
